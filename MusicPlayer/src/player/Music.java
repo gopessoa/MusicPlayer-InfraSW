3046c65ab88c701c1b307d0f62d274817a61d9c5
gopessoa <gop2@cin.ufpe.br>
2021-07-07 13:42:45 -0300

    Transformando Music em um objeto.

---
+++ MusicPlayer/src/player/Music.java
@@ -0,0 +1,16 @@
+package player;
+
+class Music {
+    public String name;
+    public String artist;
+    public String album;
+    public int duration;
+    //public image icon;
+
+    public Music(String name, String artist, String album, int duration){
+        this.name = name;
+        this.artist = artist;
+        this.album = album;
+        this.duration = duration;
+    }
+}
--- MusicPlayer/src/player/Player.java
+++ MusicPlayer/src/player/Player.java
@@ -8,58 +8,76 @@ import java.util.Scanner;
 
 class addMusicToQueue implements Runnable{
 
-    private final String music;
-    public addMusicToQueue(String music, Queue<String> queue){
+    private final Music music;
+    public addMusicToQueue(Music music, Queue<Music> queue){
         queue.add(music);
         this.music = music;
     }
 
     @Override
     public void run() {
-        System.out.printf("%s foi colocada na fila\n", music);
+        System.out.printf("%s foi colocada na fila\n", music.name);
     }
 }
 class removeMusicFromQueue implements Runnable{
 
-    private final String music;
-    public removeMusicFromQueue(String music, Queue<String> queue){
+    private final Music music;
+    public removeMusicFromQueue(Music music, Queue<Music> queue){
         queue.remove(music);
         this.music = music;
     }
 
     @Override
     public void run() {
-        System.out.printf("%s foi removida na fila\n", music);
+        System.out.printf("%s foi removida na fila\n", music.name);
     }
 }
 
 public class Player {
     public static void main(String[] args) throws InterruptedException {
         Scanner in = new Scanner(System.in);
-        Queue<String> musicQueue = new LinkedList<>();
+        Queue<Music> musicQueue = new LinkedList<>();
+        //playlist contendo musicas que podem ser adicionas á fila de reprodução
+        //Music [] playlist = new Music[5];
         //Queue<Thread> threads = new LinkedList<>();
         String command = "";
 
         while(!command.equals("CLOSE")){
-            command = in.next();
+            command = in.nextLine();
 
             if (command.equals("ADD")){
-                //System.out.println("Digite o nome da música que deseja adicionar à fila");
-                String music = in.next();
+                //System.out.println("Digite o nome da música que deseja adicionar à fila")
+                System.out.print("Nome: "); String name = in.nextLine();
+
+                System.out.print("Cantor: "); String artist = in.nextLine();
+
+                System.out.print("Album: "); String album = in.nextLine();
+
+                System.out.print("Duração: "); int duration = in.nextInt();
+
+                Music music = new Music( name, artist, album, duration);
                 Thread t = new Thread(new addMusicToQueue(music, musicQueue));
                 t.start();
             }
             if (command.equals("REMOVE")){
                 //System.out.println("Digite o nome da música que deseja remover da fila");
-                String music = in.next();
+                for (int i=0;i < musicQueue.size(); i++){
+                    System.out.printf("%s %d\n", ((LinkedList<Music>) musicQueue).get(i).name, i);
+                }
+                System.out.println("Digite o númeoro correspindente a musica que deseja remover: ");
+                Music music = ((LinkedList<Music>) musicQueue).get(in.nextInt());
                 Thread t = new Thread(new removeMusicFromQueue(music, musicQueue));
                 t.start();
             }
             if (command.equals("CHECK")){
-                System.out.println(musicQueue);
+                for (int i=0;i < musicQueue.size(); i++){
+                    System.out.printf("%s %d\n", ((LinkedList<Music>) musicQueue).get(i).name, i);
+                }
             }
         }
-        System.out.println(musicQueue);
+        for (int i=0;i < musicQueue.size(); i++){
+            System.out.printf("%s %d\n", ((LinkedList<Music>) musicQueue).get(i).name, i);
+        }
         in.close();
     }
 }
